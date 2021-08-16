96af5855ef96b13a0925c33c2857bb73b02e9f1c
Ran Leibovitz <leibovitzran@gmail.com>
2021-08-16 22:17:25 +0300

    add plus button and some features to the content

--- src/compenents/product.jsx
+++ src/compenents/product.jsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 export default class Product extends React.Component {
   constructor(props) {
@@ -36,6 +36,34 @@ export default class Product extends React.Component {
             icon={faStar}
           ></FontAwesomeIcon>
         </div>
+        <div>
+          <p>list of products for sale </p>
+        </div>
+        <div>
+          <a className="mr-5 cursor-pointer	 " src="#">
+            Leds
+          </a>
+          <a className="mr-5 cursor-pointer	 " src="#">
+            bed
+          </a>
+          <a className="mr-5 cursor-pointer	 " src="#">
+            Sleeping bag
+          </a>
+        </div>
+        <div>
+          <h1>stock quantity</h1>
+        </div>
+        <div>
+          <p>The product comes to your country</p>
+        </div>
+        <FontAwesomeIcon
+          className="text-gray-400"
+          icon={faPlus}
+        ></FontAwesomeIcon>
+        <div className="border-gray-600	border-2 w-6 h-6 text-xs items-start text-center">
+          <span>1</span>
+        </div>
+        <div>come to your home</div>
       </div>
     );
   }
