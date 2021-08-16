6f7a58284e08a31b7906d66896fea6d693973e69
Ran Leibovitz <leibovitzran@gmail.com>
2021-08-16 18:04:11 +0300

    navbar littel change

---
+++ src/compenents/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+
+export default function Footer() {
+  return <div></div>;
+}
