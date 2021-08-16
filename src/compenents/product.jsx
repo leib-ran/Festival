3b125e9ae10030aa8640eb7fe6478dcc415a13e7
Ran Leibovitz <leibovitzran@gmail.com>
2021-08-16 20:48:16 +0300

    content

--- src/compenents/product.jsx
+++ src/compenents/product.jsx
@@ -1,14 +1,41 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default class Product extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
   render() {
     return (
       <div>
         <h1>product name</h1>
+        <p>explnation of the product</p>
+        <span>price $233</span>
+        <img src="/images/tents/tent.jpg"></img>
+        <div className="flex">
+          <FontAwesomeIcon
+            className="text-gray-300 hover:text-yellow-300"
+            icon={faStar}
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            className="text-gray-300 hover:text-yellow-300"
+            icon={faStar}
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            className="text-gray-300 hover:text-yellow-300"
+            icon={faStar}
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            className="text-gray-300 hover:text-yellow-300"
+            icon={faStar}
+          ></FontAwesomeIcon>
+          <FontAwesomeIcon
+            className="text-gray-300 hover:text-yellow-300"
+            icon={faStar}
+          ></FontAwesomeIcon>
+        </div>
       </div>
     );
   }
