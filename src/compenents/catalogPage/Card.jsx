b37cdac57bf48cc28f9c8dba2d3a58336211158d
Ran Leibovitz <leibovitzran@gmail.com>
2021-08-22 09:00:06 +0300

    Links to Card

--- src/compenents/catalogPage/Card.jsx
+++ src/compenents/catalogPage/Card.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Page from "../productPage/Page";
 
 export default function Card(props) {
   return (
-    <div>
-      <div className="bg-red-800 mr-2 h-36 w-40">
-        <h1 className="h-8 text-center text-white">{props.data.nameProduct}</h1>
-        <img className="h-40" src={props.data.photo} />
-        <h1 className="bg-yellow-400">price {props.data.Usdprice}</h1>
-        <span className="block">{props.data.description}</span>
+    <Link to={`/productPage/${props.data.nameProduct}`} Component={Page}>
+      <div>
+        <div className="bg-red-800 mr-2 h-36 w-40">
+          <h1 className="h-8 text-center text-white">
+            {props.data.nameProduct}
+          </h1>
+          <img className="h-40" src={props.data.photo} />
+          <h1 className="bg-yellow-400">price {props.data.Usdprice}</h1>
+          <span className="block">{props.data.description}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
