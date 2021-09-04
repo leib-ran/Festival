5dfd204bcac2e3ead70612974b8c1478b6e65d9a
Ran Leibovitz <leibovitzran@gmail.com>
2021-09-04 19:12:40 +0300

    add category Carusel

--- src/compenents/HomePage/HomePage.jsx
+++ src/compenents/HomePage/HomePage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Carousel from "../Carousel/Carousel";
-import Globe from "./Globe";
 import WelcomingTitle from "./WelcomingTitle";
 import HomePageMap from "./HomePageMap";
+import CategoriesArea from "./CategoriesArea";
 
 export default class HomePage extends React.Component {
   constructor(props) {
@@ -11,39 +11,25 @@ export default class HomePage extends React.Component {
 
   render() {
     return (
-      <div className="relative h-screen  z-1 flex">
-        <img
-          src="/images/homePagebackground/home_page_image.jpg"
-          className="inset-0 absolute w-full z-0 filter saturate-200 hue-rotate-180 invert"
-        />
-        <div className="absolute w-full top-1/6 left-1/5 m-2">
-          <div className="absolute w-full bg-red-500 mb-2 mt-2 opacity-10"></div>
-          <div className="">
-            <div className="text-white flex">
-              <WelcomingTitle />
+      <div>
+        <div className="relative h-screen  z-1 flex">
+          <img
+            src="/images/homePagebackground/home_page_image.jpg"
+            className="inset-0 absolute w-full z-0 filter saturate-200 hue-rotate-180 invert"
+          />
+          <div className="absolute w-full top-1/6 left-1/5 m-2">
+            <div className="absolute w-full bg-red-500 mb-2 mt-2 opacity-10"></div>
+            <div className="">
+              <div className="text-white flex">
+                <WelcomingTitle />
+              </div>
             </div>
           </div>
         </div>
 
         <div className="realtive">
-          <div className="flex ">
-            {/* <Carousel>
-                <img
-                  src="/images/caruselLink/leftCarusel.jpg"
-                  className="min-w-full	"
-                  alt=""
-                />
-                <img
-                  src="/images/tents/tent.jpg"
-                  className="min-w-full	"
-                  alt=""
-                />
-                <img
-                  src="/images/caruselLink/leftCarusel.jpg"
-                  className="min-w-full	"
-                  alt=""
-                />
-              </Carousel> */}
+          <div className=" ">
+            <CategoriesArea />
           </div>
           {/* <iframe
             width="560"
@@ -55,7 +41,6 @@ export default class HomePage extends React.Component {
             allowfullscreen
           ></iframe> */}
         </div>
-        <Globe />
       </div>
     );
   }
