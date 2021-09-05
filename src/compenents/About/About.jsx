629783bf953ffa6aa0852369f8701c19cd034371
Ran Leibovitz <leibovitzran@gmail.com>
2021-09-05 09:55:46 +0300

    about me

--- src/compenents/About/About.jsx
+++ src/compenents/About/About.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 export default function About(params) {
   return (
-    <div>
+    <div className="w-full text-center">
       <h1>About us</h1>
+      <div className="flex justify-center ">
+        <p className="w-96">
+          Dionisos Company help any person from all over the world to enter to
+          Festival field. The motivation to use Dionisos is the problem of plan
+          exactly how much money are you going to spent Dionisos concentrate all
+          the products, are needed to have a good experience in festival
+          experience. the goal that ouer users will not pass their budget
+        </p>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/_tZHWmCsakk"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 }
